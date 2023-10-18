b6ecd18cd6a129ae7cd49066384cae531025ccf6
asma1m <asma.m55m@gmail.com>
2023-10-18 13:25:06 +0300

    first

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,8 @@ class MyApp extends StatelessWidget {
   
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp();
+    return GetMaterialApp(
+      title: "",
+    );
   }
 }
