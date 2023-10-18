901f8f0f7d474acfefcb3630de9df0c920038181
asma1m <asma.m55m@gmail.com>
2023-10-18 13:44:28 +0300

    theme app

---
+++ lib/core/theme/theme.dart
@@ -0,0 +1,9 @@
+import 'package:employees_app/core/theme/dark_thems.dart';
+
+import 'light_themes.dart';
+
+class ThemeApp {
+  ThemeApp._();
+  static final lightTheme = lightThemeData();
+  static final dartTheme = darkThemeData();
+}
